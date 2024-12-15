cafbbf941179fc770a31eeace6caad801b5a5b46
Carlos Mora <charlymora332@gmail.com>
2024-12-15 07:32:37 -0500

    intento de despliegue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://charlymora332.github.io/MiasFlorist',
 });
